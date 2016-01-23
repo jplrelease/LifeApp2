651b380908dd3f81f38ebc1c2ca8bd83627720a3
jkd65 <jkd65@nau.edu>
2016-01-23 14:37:47 -0700

    Added newUser.java

---
+++ app/src/main/java/com/lifeapp/newUser.java
@@ -0,0 +1,14 @@
+package com.lifeapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class newUser extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user);
+    }
+
+}
