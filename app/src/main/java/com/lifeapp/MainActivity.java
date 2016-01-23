8b7e81f5fcf6f6a6b3c04f9da2dec1813f69815e
DOhn <dao48@nau.edu>
2016-01-23 13:39:28 -0700

    Change MainAcitivity.java and add list to activity_main.xml

--- app/src/main/java/com/lifeapp/MainActivity.java
+++ app/src/main/java/com/lifeapp/MainActivity.java
@@ -1,9 +1,18 @@
 package com.lifeapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +20,60 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView HobbyList = (ListView) findViewById(R.id.list);
+        String[] hobbies = new String[] {"Rock Climbing", "Painting", "Drawing",
+        "Programming", "Dancing", "Hiking", "Reading"};
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < hobbies.length; i++) {
+            list.add(hobbies[i]);
+        }
+
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        HobbyList.setAdapter(adapter);
+
+        HobbyList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                list.remove(item);
+                                adapter.notifyDataSetChanged();
+                                view.setAlpha(1);
+                            }
+                        });
+            }
+        });
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId, List<String> objects) {
+            super(context, textViewResourceId, objects);
+
+            for (int i = 0; i < objects.size(); i++) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
     }
 
     @Override
