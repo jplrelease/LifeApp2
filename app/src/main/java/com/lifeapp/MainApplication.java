0d5ef8ead0c8860c76798f212b6fc62969628e61
knightpremier <labadie.jeanpaul@gmail.com>
2016-01-23 14:34:33 -0700

    Add Application controller

--- app/src/main/java/com/lifeapp/MainApplication.java
+++ app/src/main/java/com/lifeapp/MainApplication.java
@@ -1,7 +1,9 @@
 package com.lifeapp;
 
+import android.app.Application;
+
 /**
  * Created by Jean-Paul on 1/23/2016.
  */
-public class MainApplication {
+public class MainApplication extends Application {
 }
