68cd033d75a11fcfb4ca0df142aaacc3eec8bfa4
WAFFO <garrettsteffen@gmail.com>
2016-01-23 20:54:09 -0700

    Clear user field on tap

--- app/src/main/java/com/lifeapp/newUser.java
+++ app/src/main/java/com/lifeapp/newUser.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.EditText;
 
 public class newUser extends Activity {
 
@@ -17,5 +18,9 @@ public class newUser extends Activity {
         Intent intent = new Intent(newUser.this, Questionaire.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         startActivity(intent);
+    }
+    public void clickUsername(View v){
+        EditText feild = (EditText) findViewById(R.id.editText);
+        feild.setText("");
     }
 }
