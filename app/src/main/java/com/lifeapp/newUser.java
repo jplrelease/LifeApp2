e14c1d66142a9f951c69c37c47005894c32372ed
awseale <awseale@email.arizona.edu>
2016-01-23 20:22:56 -0700

    added button

--- app/src/main/java/com/lifeapp/newUser.java
+++ app/src/main/java/com/lifeapp/newUser.java
@@ -1,7 +1,9 @@
 package com.lifeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 public class newUser extends Activity {
 
@@ -10,5 +12,14 @@ public class newUser extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
     }
+
+        public void andrewClick(View v)
+        {
+            Intent intent = new Intent(newUser.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(intent);
+        }
+
+
 
 }
