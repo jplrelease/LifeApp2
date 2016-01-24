e4086b88fa1cfb0665cf61261d5b064842dc50e5
DOhn <dao48@nau.edu>
2016-01-23 22:10:04 -0700

    Fix New User text editor in UserCreationController.

--- app/src/main/java/com/lifeapp/UserCreationController.java
+++ app/src/main/java/com/lifeapp/UserCreationController.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class UserCreationController extends Activity {
 
@@ -14,8 +15,11 @@ public class UserCreationController extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
 
-        Button button = (Button)findViewById(R.id.userButton);
+        String str = "Insert Username";
+        EditText username = (EditText)findViewById(R.id.userText);
+        username.setText(str, TextView.BufferType.EDITABLE);
 
+        Button button = (Button)findViewById(R.id.userButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,10 +27,4 @@ public class UserCreationController extends Activity {
             }
         });
     }
-    
-    public void clickUsername(View v){
-        EditText feild = (EditText) findViewById(R.id.editText);
-        feild.setText("");
-    }
-
 }
