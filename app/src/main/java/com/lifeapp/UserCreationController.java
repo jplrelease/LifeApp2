bd02ac3e1e85943d1d1defb4ff01bbf44fc3d958
DOhn <dao48@nau.edu>
2016-01-23 21:43:24 -0700

    Fix user button in UserCreationController.

--- app/src/main/java/com/lifeapp/UserCreationController.java
+++ app/src/main/java/com/lifeapp/UserCreationController.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class UserCreationController extends Activity {
@@ -12,13 +13,17 @@ public class UserCreationController extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+
+        Button button = (Button)findViewById(R.id.userButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UserCreationController.this, QuestionaireController.class));
+            }
+        });
     }
-    public void andrewClick(View v)
-    {
-        Intent intent = new Intent(UserCreationController.this, QuestionaireController.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-        startActivity(intent);
-    }
+    
     public void clickUsername(View v){
         EditText feild = (EditText) findViewById(R.id.editText);
         feild.setText("");
